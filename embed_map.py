600fc9a28d193b99f6df1d51c8925880f6db3754
vonstackelberg-hell <vonstackelberg@gmail.com>
2026-05-24 22:57:44 +0200

    Add files via upload
    Initial setup

---
+++ embed_map.py
@@ -0,0 +1,17 @@
+import json
+
+with open('ihco_systems.json', 'r', encoding='utf-8') as f:
+    data = json.load(f)
+
+with open('ihco_map.html', 'r', encoding='utf-8') as f:
+    html = f.read()
+
+html = html.replace(
+    "fetch('ihco_systems.json')\n  .then(r => r.json())\n  .then(data => {",
+    f"Promise.resolve({json.dumps(data)})\n  .then(data => {{"
+)
+
+with open('ihco_map_embedded.html', 'w', encoding='utf-8') as f:
+    f.write(html)
+
+print("Fertig! Öffne ihco_map_embedded.html")
